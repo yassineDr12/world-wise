f1fa6225fcca4356b251a122ad7ba6e406fad0b2
Yassine Drafate <yassinedrafate1@gmail.com>
2024-08-24 17:09:48 +0100

    update footer for consistent theming

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,30 +1,32 @@
-import { Box, Container, Typography, Link } from "@mui/material";
+import { Box, Container, Typography, Link, AppBar } from "@mui/material";
 
 const Footer = () => {
   return (
-    <Box component="footer" sx={{ bgcolor: "primary.main", py: 3, color: "white" }}>
-      <Container maxWidth="lg">
-        <Typography variant="body1" align="center">
-          © {new Date().getFullYear()} WorldWise - Keep track of your adventures
-        </Typography>
-        <Typography variant="body2" align="center" sx={{ mt: 1 }}>
-          Made with ❤️ by
-          <Link href="https://github.com/yassineDr12" target="_blank" rel="noopener" color="inherit" sx={{ ml: 1 }}>
-            Yassine Drafate
-          </Link>{" "}
-          |
-          <Link
-            href="https://github.com/yassineDr12/world-wise"
-            target="_blank"
-            rel="noopener"
-            color="inherit"
-            sx={{ ml: 1 }}
-          >
-            GitHub
-          </Link>
-        </Typography>
-      </Container>
-    </Box>
+    <AppBar position="static" color="primary" elevation={10}>
+      <Box display="flex" alignItems="center" height="80px">
+        <Container maxWidth="lg">
+          <Typography variant="body1" align="center">
+            © {new Date().getFullYear()} WorldWise - Keep track of your adventures
+          </Typography>
+          <Typography variant="body2" align="center" sx={{ mt: 1 }}>
+            Made with ❤️ by
+            <Link href="https://github.com/yassineDr12" target="_blank" rel="noopener" color="inherit" sx={{ ml: 1 }}>
+              Yassine Drafate
+            </Link>{" "}
+            |
+            <Link
+              href="https://github.com/yassineDr12/world-wise"
+              target="_blank"
+              rel="noopener"
+              color="inherit"
+              sx={{ ml: 1 }}
+            >
+              GitHub
+            </Link>
+          </Typography>
+        </Container>
+      </Box>
+    </AppBar>
   );
 };
 
